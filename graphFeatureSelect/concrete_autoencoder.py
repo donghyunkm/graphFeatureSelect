d6e229f756e39cba60448c6767274969b1d7d1d2
dkim195 <donghyun.ethan.kim@gmail.com>
2024-09-06 21:32:52 +0000

    First commit

---
+++ graphFeatureSelect/concrete_autoencoder.py
@@ -0,0 +1,82 @@
+# https://github.com/pragyasingh7/Concrete-AutoEncoder-Pytorch
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class ConcreteEncoder(nn.Module):
+    def __init__(self, input_dim, output_dim, start_temp=10.0, min_temp=0.01, alpha=0.99999):
+        super().__init__()
+        self.start_temp = start_temp
+        self.min_temp = min_temp
+        self.alpha = alpha
+
+        self.temp = start_temp
+        self.logits = nn.Parameter(torch.empty(output_dim, input_dim))
+        nn.init.xavier_normal_(self.logits)
+
+    def forward(self, X, train=True, X_mask=None, debug=False):
+        uniform = torch.rand(self.logits.shape).clamp(min=1e-7)
+        gumbel = -torch.log(-torch.log(uniform))
+        self.temp = max([self.temp * self.alpha, self.min_temp])
+        noisy_logits = (self.logits + gumbel) / self.temp
+
+        if X_mask is not None:
+            X *= X_mask
+            logits_mask = X_mask.int() ^ 1
+            noisy_logits = noisy_logits.reshape(1, self.logits.shape[0], -1)
+            noisy_logits = torch.add(noisy_logits, logits_mask, alpha=-1e7)
+
+        samples = F.softmax(noisy_logits, dim=-1)
+
+        discrete_logits = F.one_hot(torch.argmax(self.logits, dim=-1), self.logits.shape[1]).float()
+
+        selection = samples if train else discrete_logits
+
+        Y = torch.matmul(X, torch.transpose(selection, -1, -2))
+
+        if debug:
+            return X, selection
+
+        return Y
+
+
+class Decoder(nn.Module):
+    def __init__(self, decoder_type, input_dim, output_dim):
+        super().__init__()
+        if decoder_type == 'lr':
+            self.decoder = nn.Linear(input_dim, output_dim)
+        elif decoder_type == 'mlp':
+            self.decoder = nn.Sequential(
+                nn.Linear(input_dim, 320), nn.LeakyReLU(), nn.Dropout(0.1),
+                nn.Linear(320, 320), nn.LeakyReLU(), nn.Dropout(0.1),
+                nn.Linear(320, output_dim)
+            )
+        else:
+            raise ValueError(f'Unsupported decoder type: {decoder_type}')
+
+    def forward(self, X):
+        return self.decoder(X)
+
+
+class ConcreteAutoEncoder(nn.Module):
+    def __init__(self, input_dim, k, start_temp=10.0, min_temp=0.01, alpha=0.99999, decoder_type='lr'):
+        super().__init__()
+        self.encoder = ConcreteEncoder(input_dim, k, start_temp=start_temp, min_temp=min_temp, alpha=alpha)
+        self.decoder = Decoder(decoder_type, k, input_dim)
+
+    def get_mean_max(self):
+        return torch.mean(torch.max(F.softmax(self.encoder.logits, -1)))
+
+    def get_prob(self):
+        return F.softmax(self.encoder.logits, -1)
+
+    def get_indices(self):
+        return torch.argmax(self.encoder.logits, dim=-1)
+
+    def forward(self, X, train=True, X_mask=None, debug=False):
+        selected_features = self.encoder(X, train=train, X_mask=X_mask, debug=debug)
+        outputs = self.decoder(selected_features)
+        if train and X_mask is not None:
+            outputs *= X_mask
+        return outputs
---
+++ graphFeatureSelect/utils.py
@@ -0,0 +1,98 @@
+# https://github.com/pragyasingh7/Concrete-AutoEncoder-Pytorch
+import torch
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+from graphFeatureSelect.concrete_autoencoder import ConcreteAutoEncoder
+
+
+def jaccard_index(a, b):
+    a = set(a.numpy()) if type(a) != set else a
+    b = set(b.numpy()) if type(b) != set else b
+    a_and_b = a.intersection(b)
+    a_or_b = a.union(b)
+
+    return len(a_and_b) / len(a_or_b)
+
+
+def train_masked_mean(dt, train_idx):
+    x, x_mask = dt.data[train_idx].squeeze(), dt.data_mask[train_idx].squeeze()
+    masked_x = x * x_mask
+    masked_mean = torch.sum(masked_x, dim=0) / torch.sum(x_mask, dim=0)
+
+    return masked_mean
+
+
+def impute_mean(x, x_mask, masked_mean):
+    inv_mask = x_mask.int() ^ 1
+    impute_value = inv_mask * masked_mean
+    x = x + impute_value
+
+    return x
+
+
+def load_model(file_nm, input_dim, k, alpha=0.99999, decoder_type='mlp'):
+    model = ConcreteAutoEncoder(input_dim, k, alpha=alpha, decoder_type=decoder_type)
+    model.load_state_dict(torch.load(file_nm)['model_state_dict'])
+    return model
+
+
+def create_mnist_figures(model, images, image_masks=None, folder='./data/figures/', tag=''):
+    samples_per_feature = [1, 3]
+    probs = model.get_prob()
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    for i in samples_per_feature:
+        top = torch.topk(probs, i)
+        im = torch.zeros(28 * 28)
+        c = i
+        for idx in top.indices.transpose(0, 1):
+            im[idx] = c
+            c -= 1
+        plt.imshow(torch.reshape(im, (28, 28)))
+        plt.savefig(f'{folder}/selected{tag}_{i}.png')
+
+    fig, axs = plt.subplots((len(images) * 2 + 4) // 5, 5)
+    for i, ax in enumerate(axs.flatten()[:10]):
+        im = images[i] if image_masks is None else images[i]*image_masks[i]
+        ax.imshow(torch.reshape(im, (28, 28)))
+    for i, ax in enumerate(axs.flatten()[10:]):
+        X_mask = image_masks[i] if image_masks is not None else None
+        pred = model(images[i], train=False, X_mask=X_mask)
+        ax.imshow(np.reshape(pred.detach().numpy(), (28, 28)))
+    plt.savefig(f'{folder}/reconstructed{tag}.png')
+
+
+def plot_performance(losses, folder='./data/figures/', tag=''):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    _, ax = plt.subplots()
+    for key, loss in losses.items():
+        ax.plot(loss, label=key)
+    ax.set_xlabel('Number of epochs')
+    ax.set_ylabel('MSE loss')
+    ax.legend()
+    ax.grid()
+    plt.savefig(f'{folder}/performance{tag}.png')
+
+
+def plot_annealing_schedule(df, folder='./data/figures/', tag=''):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    fig, ax = plt.subplots()
+    axes = [ax, ax.twinx(), ax.twinx()]
+    fig.subplots_adjust(right=0.75)
+    axes[-1].spines['right'].set_position(('axes', 1.2))
+    axes[-1].set_frame_on(True)
+    axes[-1].patch.set_visible(False)
+
+    colors = ('Green', 'Red', 'Blue')
+    for ax, color, key in zip(axes, colors, list(df.keys())):
+        ax.plot(df[key], color=color)
+        ax.set_ylabel(key, color=color)
+        ax.tick_params(axis='y', colors=color)
+    axes[0].set_xlabel('Number of epochs')
+    plt.savefig(f'{folder}/annealing{tag}.png')
