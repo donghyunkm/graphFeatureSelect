1778cf015a46344dbdff2fdf3d699ade87602c0a
rhngla <rhngla@gmail.com>
2025-01-30 11:00:50 -0800

    updates to data schema (node features are now all in data.x), and more control over train/val splits

--- gfs/data/hemisphere.py
+++ gfs/data/hemisphere.py
@@ -16,6 +16,142 @@ from gfs.utils import get_paths
 warnings.filterwarnings("ignore", category=ImplicitModificationWarning, message="Transforming to str index.")
 
 
+def read_check_h5ad(path):
+    adata = ad.read_h5ad(path)
+    for field in ["spatial_connectivities", "spatial_distances"]:
+        assert field in adata.obsp.keys(), f"{field} absent: Run `sc.pp.neighbors` first for {path}"
+    return adata
+
+
+def get_non_blank_genes(adata):
+    keep_genes = adata.var[~adata.var.index.str.startswith("Blank")].index
+    return keep_genes
+
+
+class PyGAnnData:
+    """
+    Class to preprocess and build the PyG Data object from AnnData dataset
+
+    Args:
+        paths (list): List of paths to the AnnData files.
+        keep_genes (list): List of genes to keep.
+        keep_cells (list): List of cells to keep.
+        spatial_coords (list): Anndata.obs columns for spatial coordinates (for regression).
+        cell_type (str): Anndata.obs column for cell types.
+        d_threshold (float): Distance threshold (in mm) for considering neighbors.
+        rand_seed (int): Random seed for reproducibility of train/val split.
+    """
+
+    def __init__(
+        self,
+        paths=[],
+        keep_genes=None,
+        keep_cells=None,
+        spatial_coords=["x_ccf", "y_ccf", "z_ccf"],
+        cell_type="supertype",
+        d_threshold=1000,
+        n_splits=5,
+        cv=0,
+        rand_seed=42,
+    ):
+        super().__init__()
+        self.paths = paths
+
+        adata = read_check_h5ad(self.paths[0])
+        if len(self.paths) > 1:
+            for i in range(1, len(self.paths)):
+                adata = ad.concat(
+                    [adata, read_check_h5ad(self.paths[i])], axis=0, join="inner", merge="same", pairwise=True
+                )
+
+        # filter genes
+        if keep_genes is not None:
+            adata = adata[:, keep_genes].copy()
+        else:
+            keep_genes = get_non_blank_genes(adata)
+            adata = adata[:, keep_genes].copy()
+
+        # filter cells
+        if keep_cells is not None:
+            adata = adata[keep_cells, :].copy()
+
+        self.adata = adata
+        self.d_threshold = d_threshold
+
+        # calculate binary adjacency matrix
+        adj = self.adata.obsp["spatial_connectivities"].copy()
+        adj = adj.astype(bool).astype(int)
+        adj.setdiag(0)  # removing self-loops
+        self.adj = adj
+
+        self.spatial_coords = spatial_coords
+        self.cell_type = cell_type
+        self.cell_type_list = adata.obs[cell_type].cat.categories.tolist()
+        self.cell_type_labelencoder = LabelEncoder()
+        self.cell_type_labelencoder.fit(self.cell_type_list)
+        self.data_issparse = issparse(adata.X)
+
+        # reproducible train/val split for crossvalidation 5 folds using StratifiedKFold
+        self.cv = cv
+        self.n_splits = n_splits
+        assert self.cv < self.n_splits, "Crossvalidation index out of range"
+        skf = StratifiedKFold(n_splits=self.n_splits, shuffle=True, random_state=rand_seed)
+        splits = skf.split(self.adata, self.adata.obs[self.cell_type])
+        splits = list(splits)
+        self.train_ind = splits[self.cv][0]
+        self.val_ind = splits[self.cv][1]
+
+    def convert_torch_sparse_coo(self, adj):
+        coo_matrix = adj.tocoo()
+        indices = np.vstack((coo_matrix.row, coo_matrix.col))
+        values = coo_matrix.data
+        shape = coo_matrix.shape
+
+        indices = torch.tensor(indices, dtype=torch.int64)
+        values = torch.tensor(values, dtype=torch.float32)
+        sparse_tensor = torch.sparse_coo_tensor(indices, values, torch.Size(shape))
+        return sparse_tensor._indices()
+
+    def get_pygdata_obj(self):
+        """Returns a `torch_geometric.data.Data` object.
+        This implementation creates tensors from the full adata object.
+        `edge_index` and `num_nodes` are required for NeighborLoader to work correctly."""
+
+        if self.data_issparse:
+            gene_exp = torch.tensor(self.adata.X.toarray()).float()
+        else:
+            gene_exp = torch.tensor(self.adata.X).float()
+
+        edgelist = self.convert_torch_sparse_coo(self.adj)
+
+        celltype = self.cell_type_labelencoder.transform(
+            self.adata.obs.iloc[[i for i in range(self.adata.shape[0])]][self.cell_type]
+        )
+        celltype = torch.tensor(celltype).long()
+
+        xyz = torch.tensor(self.adata.obs[self.spatial_coords].values).float()
+
+        # boolean masks for train/val
+        train_mask = torch.zeros(self.adata.shape[0], dtype=torch.bool)
+        train_mask[self.train_ind] = True
+        val_mask = torch.zeros(self.adata.shape[0], dtype=torch.bool)
+        val_mask[self.val_ind] = True
+
+        x = torch.cat([gene_exp, xyz], dim=1)
+        gene_exp_ind = torch.arange(gene_exp.shape[1])
+        xyz_ind = torch.arange(gene_exp.shape[1], gene_exp.shape[1] + xyz.shape[1])
+        return PyGData(
+            x=x,
+            edge_index=edgelist,
+            celltype=celltype,
+            num_nodes=gene_exp.shape[0],
+            train_mask=train_mask,
+            val_mask=val_mask,
+            gene_exp_ind=gene_exp_ind,
+            xyz_ind=xyz_ind
+        )
+
+
 class PyGAnnDataGraphDataModule(L.LightningDataModule):
     """
     Data module using PyG functions to return graph patches.
@@ -88,139 +224,6 @@ class PyGAnnDataGraphDataModule(L.LightningDataModule):
         )
 
 
-def read_check_h5ad(path):
-    adata = ad.read_h5ad(path)
-    for field in ["spatial_connectivities", "spatial_distances"]:
-        assert field in adata.obsp.keys(), f"{field} absent: Run `sc.pp.neighbors` first for {path}"
-    return adata
-
-
-def get_non_blank_genes(adata):
-    keep_genes = adata.var[~adata.var.index.str.startswith("Blank")].index
-    return keep_genes
-
-
-class PyGAnnData:
-    """
-    Class to preprocess and build the PyG Data object from AnnData dataset
-
-    Args:
-        path (str): Path to the AnnData file.
-        keep_genes (list): List of genes to keep.
-        keep_cells (list): List of cells to keep.
-        spatial_coords (list): Anndata.obs columns for spatial coordinates (for regression).
-        cell_type (str): Anndata.obs column for cell types.
-        max_order (int): Maximum order of neighbors to consider.
-        d_threshold (float): Distance threshold (in mm) for considering neighbors.
-    """
-
-    def __init__(
-        self,
-        paths=[],
-        keep_genes=None,
-        keep_cells=None,
-        spatial_coords=["x_ccf", "y_ccf", "z_ccf"],
-        cell_type="supertype",
-        max_order=2,
-        d_threshold=1000,
-        rand_seed=42,
-    ):
-        super().__init__()
-        self.paths = paths
-
-        adata = read_check_h5ad(self.paths[0])
-        if len(self.paths) > 1:
-            for i in range(1, len(self.paths)):
-                adata = ad.concat(
-                    [adata, read_check_h5ad(self.paths[i])], axis=0, join="inner", merge="same", pairwise=True
-                )
-
-        # filter genes
-        if keep_genes is not None:
-            adata = adata[:, keep_genes].copy()
-        else:
-            keep_genes = get_non_blank_genes(adata)
-            adata = adata[:, keep_genes].copy()
-
-        # filter cells
-        if keep_cells is not None:
-            adata = adata[keep_cells, :].copy()
-
-        self.adata = adata
-        self.max_order = max_order
-        self.d_threshold = d_threshold
-
-        # create binary adjacency matrix without self-loops
-        adj = self.adata.obsp["spatial_connectivities"].copy()
-        adj = adj.astype(bool).astype(int)
-        # this is an extra precaution to prevent far connections.
-        adj[self.adata.obsp["spatial_distances"] > self.d_threshold] = 0
-        adj.setdiag(0)
-        self.adj = adj
-
-        self.spatial_coords = spatial_coords
-        self.cell_type = cell_type
-        self.cell_type_list = adata.obs[cell_type].cat.categories.tolist()
-        self.cell_type_labelencoder = LabelEncoder()
-        self.cell_type_labelencoder.fit(self.cell_type_list)
-        self.data_issparse = issparse(adata.X)
-
-        # reproducible train/val/test split for crossvalidation 5 folds using StratifiedKFold
-        self.cv = 0
-        skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=rand_seed)
-        splits = skf.split(self.adata, self.adata.obs[self.cell_type])
-        splits = list(splits)
-        self.train_ind = splits[self.cv][0]
-        self.val_ind = splits[self.cv][1]
-
-    def convert_torch_sparse_coo(self, adj):
-        coo_matrix = adj.tocoo()
-        indices = np.vstack((coo_matrix.row, coo_matrix.col))
-        values = coo_matrix.data
-        shape = coo_matrix.shape
-
-        indices = torch.tensor(indices, dtype=torch.int64)
-        values = torch.tensor(values, dtype=torch.float32)
-        sparse_tensor = torch.sparse_coo_tensor(indices, values, torch.Size(shape))
-        return sparse_tensor._indices()
-
-    def get_pygdata_obj(self):
-        """Returns a `torch_geometric.data.Data` object.
-        This implementation creates tensors from the full adata object.
-        `edge_index` and `num_nodes` are required for NeighborLoader to work correctly."""
-
-        if self.data_issparse:
-            gene_exp = torch.tensor(self.adata.X.toarray()).float()
-        else:
-            gene_exp = torch.tensor(self.adata.X).float()
-
-        edgelist = self.convert_torch_sparse_coo(self.adj)
-
-        celltype = self.cell_type_labelencoder.transform(
-            self.adata.obs.iloc[[i for i in range(self.adata.shape[0])]][self.cell_type]
-        )
-        celltype = torch.tensor(celltype).long()
-
-        xyz = torch.tensor(self.adata.obs[self.spatial_coords].values).float()
-
-        # boolean mask for train/val/test
-        # first, define all false
-        train_mask = torch.zeros(self.adata.shape[0], dtype=torch.bool)
-        train_mask[self.train_ind] = True
-        val_mask = torch.zeros(self.adata.shape[0], dtype=torch.bool)
-        val_mask[self.val_ind] = True
-
-        return PyGData(
-            gene_exp=gene_exp,
-            edge_index=edgelist,
-            xyz=xyz,
-            celltype=celltype,
-            num_nodes=gene_exp.shape[0],
-            train_mask=train_mask,
-            val_mask=val_mask,
-        )
-
-
 def test_pyganndatagraphdatamodule():
     import numpy as np
 
@@ -271,7 +274,6 @@ def test_pyganndata():
         keep_cells=None,
         spatial_coords=["x_ccf", "y_ccf", "z_ccf"],
         cell_type="supertype",
-        max_order=2,
         d_threshold=1000,
     )
     print("pyganndata checks passed")
